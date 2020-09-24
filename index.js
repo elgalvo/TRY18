b5db208f9a126605098125516403a5fe7b50add5
Luan <luan@brindel.com.br>
2020-09-24 10:30:03 -0300

    Banco de Dados Configurado

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 //MONGOOOSE
-mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify:false}, (error)=>{
+mongoose.connect("mongodb://brindelmanager:capivara:mongo_brindelsistema:27017/brindelsistema", {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify:false}, (error)=>{
     if(error){
         console.log(error)
     } else {
